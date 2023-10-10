d354e7c6047c58b5e594e0a55ed149454d20ec0c
CaraChaato <pedrovinic12@gmail.com>
2023-10-10 16:52:30 -0300

    Teste de Merge

--- src/MainProjeto.java
+++ src/MainProjeto.java
@@ -1,5 +1,10 @@
 public class MainProjeto {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        queroCafe();
+    }
+
+    public static void queroCafe(){
+        System.out.println("Quero caféee!!!");
     }
 }
