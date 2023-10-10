20d467ca345213a146672eaf1a455eb70479162f
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-10-10 16:51:36 -0300

    teste

--- src/MainProjeto.java
+++ src/MainProjeto.java
@@ -1,5 +1,11 @@
 public class MainProjeto {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        aquiHeNathan();
+    }
+
+    public static void aquiHeNathan(){
+        System.out.println("Eu escrevi isso");
     }
 }
