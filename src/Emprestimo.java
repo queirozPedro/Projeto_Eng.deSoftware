d8ce1c01251b5d8fec56c67631afddde14223d25
gustav0lb <gustavoflamengo357@gmail.com>
2023-10-18 15:54:23 -0300

    Update Emprestimo

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -43,14 +43,36 @@ public class Emprestimo {
 
     // Método para confirmar a renovação
     public boolean confirmarRenovacao() {
-        // Lógica para confirmar ou negar a renovação do empréstimo.
-        return true; // Retorna true se a renovação foi confirmada, ou false se negada.
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        PreparedStatement state;
+
+        try {
+            // Atualiza a data_final do empréstimo para uma nova data
+            String query = "UPDATE emprestimo SET data_final = ? WHERE id_aluno = ? AND id_livro = ? AND data_final = ?";
+            state = connection.prepareStatement(query);
+            state.setDate(1, new java.sql.Date(dataFinal.getTime())); // Nova data final
+            state.setInt(2, aluno.getMatricula());
+            state.setInt(3, idLivro);
+            state.setDate(4, new java.sql.Date(dataInicial.getTime())); // Data inicial original
+            int updatedRows = state.executeUpdate();
+
+            return updatedRows > 0; // Retorna true se a renovação foi confirmada, ou false se negada.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Renovação não realizada
     }
 
     // Método para calcular penalização por devolução fora do prazo
-    public void calculoPenalizacao(Date dataPrazo, Date dataFinal) {
-        // Lógica para calcular e aplicar a penalização com base nas datas.
-    }
+    public void calculoPenalizacao(Date dataPrazo, Date dataDevolucao) {
+        long diff = dataDevolucao.getTime() - dataPrazo.getTime();
+        long diasDeAtraso = diff / (24 * 60 * 60 * 1000); // Converter milissegundos em dias
 
-    // Outros métodos e getters/setters
+        // Lógica para calcular e aplicar a penalização com base nos dias de atraso.
+        if (diasDeAtraso > 0) {
+            // Aplicar penalização, como multa, de acordo com as regras da biblioteca.
+            // Você pode adicionar lógica específica aqui.
+        }
+    }
 }
