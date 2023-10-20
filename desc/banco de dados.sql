e703a75d27dec4df905bc48744942b915aea11b7
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-10-19 21:43:13 -0300

    Update banco de dados.sql

--- desc/banco de dados.sql
+++ desc/banco de dados.sql
@@ -4,21 +4,23 @@
 
 CREATE TABLE IF NOT EXISTS public.usuario
 (
-    matricula integer NOT NULL,
+    matricula integer NOT NULL DEFAULT 'nextval('usuario_matricula_seq'::regclass)',
     nome character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    cpf character(11) COLLATE pg_catalog."default" NOT NULL,
+    cpf character varying(20) COLLATE pg_catalog."default" NOT NULL,
     email character varying(100) COLLATE pg_catalog."default" NOT NULL,
     idade integer NOT NULL,
     senha character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    telefone character(11) COLLATE pg_catalog."default" DEFAULT 'NULL::bpchar',
+    telefone character varying(20) COLLATE pg_catalog."default",
     bibliotecario boolean NOT NULL,
-    CONSTRAINT aluno_pkey PRIMARY KEY (matricula)
+    CONSTRAINT usuario_pkey PRIMARY KEY (matricula)
+)
+WITH (
+    OIDS = FALSE
 )
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.usuario
     OWNER to qaokqcmt;
-
 
 -- Table: public.livro
 
@@ -28,11 +30,14 @@ CREATE TABLE IF NOT EXISTS public.livro
 (
     id_livro integer NOT NULL DEFAULT 'nextval('livro_id_livro_seq'::regclass)',
     titulo character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    editora character varying(100) COLLATE pg_catalog."default" NOT NULL,
     autor character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    n_paginas integer NOT NULL,
+    editora character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    npaginas integer NOT NULL,
     quantidade integer NOT NULL,
     CONSTRAINT livro_pkey PRIMARY KEY (id_livro)
+)
+WITH (
+    OIDS = FALSE
 )
 TABLESPACE pg_default;
 
@@ -45,23 +50,25 @@ ALTER TABLE IF EXISTS public.livro
 
 CREATE TABLE IF NOT EXISTS public.emprestimo
 (
-    id_usuario integer NOT NULL,
+    matricula integer NOT NULL,
     id_livro integer NOT NULL,
-    datainicial date NOT NULL,
-    datafinal date NOT NULL,
-    datadevolucao date,
-    status character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT emprestimo_pkey PRIMARY KEY (id_usuario, id_livro),
+    data_inicial date,
+    data_final date,
+    data_devolucao date,
+    status character varying(30) COLLATE pg_catalog."default",
+    CONSTRAINT emprestimo_pkey PRIMARY KEY (matricula, id_livro),
     CONSTRAINT emprestimo_id_livro_fkey FOREIGN KEY (id_livro)
         REFERENCES public.livro (id_livro) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT emprestimo_id_usuario_fkey FOREIGN KEY (id_usuario)
+    CONSTRAINT emprestimo_matricula_fkey FOREIGN KEY (matricula)
         REFERENCES public.usuario (matricula) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
-
+WITH (
+    OIDS = FALSE
+)
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.emprestimo
@@ -74,15 +81,17 @@ ALTER TABLE IF EXISTS public.emprestimo
 CREATE TABLE IF NOT EXISTS public.penalidade
 (
     matricula integer NOT NULL,
-    fimpenalidade date NOT NULL,
-    motivo character varying(300) COLLATE pg_catalog."default" NOT NULL,
+    fim_penalidade date,
+    motivo character varying(300) COLLATE pg_catalog."default",
     CONSTRAINT penalidade_pkey PRIMARY KEY (matricula),
     CONSTRAINT penalidade_matricula_fkey FOREIGN KEY (matricula)
         REFERENCES public.usuario (matricula) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
-
+WITH (
+    OIDS = FALSE
+)
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.penalidade
